451f7a509e7298d9be616abb840ad687ae377cda
NightlyHerb <82109216+nightlyherb@users.noreply.github.com>
2022-11-01 15:18:42 +0900

    feat: polish rating radio button

--- src/App/RatingSelectCard/index.tsx
+++ src/App/RatingSelectCard/index.tsx
@@ -4,36 +4,53 @@ import styles from "./styles.module.scss";
 
 export default function RatingSelectCard(props: {
   selectedRating: Rating | null;
+  onSelect: (selectedRating: Rating) => void;
   name: string;
 }) {
+  const { selectedRating, onSelect, name } = props;
+  const allRatings = [1, 2, 3, 4, 5] as Rating[];
   return (
     <div className={styles.mainContainer}>
-      <RatingRadioButton isSelected={false} name={props.name} rating={1} />
-      <RatingRadioButton isSelected={false} name={props.name} rating={2} />
-      <RatingRadioButton isSelected={false} name={props.name} rating={3} />
-      <RatingRadioButton isSelected={false} name={props.name} rating={4} />
-      <RatingRadioButton isSelected={false} name={props.name} rating={5} />
+      {allRatings.map((i) => {
+        return (
+          <RatingRadioButton
+            key={i}
+            isSelected={i === selectedRating}
+            onSelect={onSelect}
+            name={name}
+            rating={i}
+          />
+        );
+      })}
     </div>
   );
 }
 
 function RatingRadioButton(props: {
   isSelected: boolean;
+  onSelect: (rating: Rating) => void;
   name: string;
   rating: Rating;
 }) {
+  const { isSelected, onSelect, name, rating } = props;
   return (
     <label
-      htmlFor={`${props.name}-${props.rating}`}
-      className={styles.radioLabel}
+      htmlFor={`${name}-${rating}`}
+      className={`${styles.radioLabel} ${
+        isSelected && styles.selectedRadioLabel
+      }`}
     >
       <input
         type="radio"
-        name={props.name}
-        id={`${props.name}-${props.rating}`}
+        name={name}
+        id={`${name}-${rating}`}
         className={styles.radioInput}
+        checked={isSelected}
+        onChange={() => {
+          onSelect(rating);
+        }}
       />
-      {props.rating}
+      {rating}
     </label>
   );
 }
